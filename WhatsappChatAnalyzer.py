07cff06a1e864cadf85bc207b788c8d02170192d
M SREEJITH <85403367+sreejith2005@users.noreply.github.com>
2024-04-09 16:45:09 +0530

    Update WhatsappChatAnalyzer.py

--- WhatsappChatAnalyzer.py
+++ WhatsappChatAnalyzer.py
@@ -4,8 +4,9 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 st.title("Whatsapp Chat Analyzer")
+st.sidebar.title("Upload a File")
 
-uploaded_file = st.sidebar.file_uploader("Choose a file")
+# uploaded_file = st.sidebar.file_uploader("Choose a file")
 if uploaded_file is not None:
     bytes_data = uploaded_file.getvalue()
     data = bytes_data.decode("utf-8")
